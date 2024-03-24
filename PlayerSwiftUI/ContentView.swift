6611588f4d7fad0bab42eab958c243a620deca87
walewy <walewy@yandex.ru>
2024-03-24 12:41:36 +0300

    first commit

---
+++ PlayerSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlayerSwiftUI
+//
+//  Created by Александр Калашников on 24.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlayerSwiftUI/PlayerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerSwiftUIApp.swift
+//  PlayerSwiftUI
+//
+//  Created by Александр Калашников on 24.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct PlayerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
